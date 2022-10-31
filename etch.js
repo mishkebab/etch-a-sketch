67b023da78856fd6dfb6516c1cb5e1a00a2caa8f
misha <mishabansal7@gmail.com>
2022-10-31 12:48:56 -0700

    giving user grid size option

--- etch.js
+++ etch.js
@@ -2,26 +2,36 @@ console.log("Hello World");
 
 const container = document.querySelector('.container');
 
-for (i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    row.textContent = "";
+const gridsize = document.querySelector('.gridsize');
 
-    container.appendChild(row);
-
-    for (j = 0; j < 16; j++) {
-        const column = document.createElement('div');
-        column.classList.add('column');
-        column.textContent = "";
-
-        row.appendChild(column);
-
-        column.addEventListener("mouseover", () => changeColor(column));
-    }
-}
 
 function changeColor(div) {
     let randomColor = Math.floor(Math.random()*16777215).toString(16);
     div.style.backgroundColor = "#" + randomColor;
     console.log
 }
+
+function createGrid(){
+    let size = prompt("Choose a number between 10-30");
+
+    for (i = 0; i < size; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        row.textContent = "";
+    
+        container.appendChild(row);
+    
+        for (j = 0; j < size; j++) {
+            const column = document.createElement('div');
+            column.classList.add('column');
+            column.textContent = "";
+    
+            row.appendChild(column);
+    
+            column.addEventListener("mouseover", () => changeColor(column));
+        }
+    }
+}
+
+
+
